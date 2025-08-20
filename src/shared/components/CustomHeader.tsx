fa3bbfde2566e048219ae51328c8d1c6d83aa6f7
Esteban Russo <oerusso28@gmail.com>
2025-08-20 15:32:33 -0300

    refactor: update stickers app layout and styles, rename CSS classes for clarity

---
+++ src/shared/components/CustomHeader.tsx
@@ -0,0 +1,12 @@
+interface CustomHeaderProps {
+  title: string;
+  description? : string;
+}
+export const CustomHeader = ({ title, description }:CustomHeaderProps) => {
+  return (
+    <div className="content-center">
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </div>
+  );
+};
